5030c78e93d4b31cae96bca5eaae50ed4f5e6996
iwagaki <iwagaki@users.sourceforge.net>
2012-07-15 01:54:17 +0900

    added DirUtil

---
+++ dirutil.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+class DirUtil:
+    dir_list = []
+
+    @staticmethod
+    def pushd(path):
+        DirUtil.dir_list.append(os.getcwd())
+        os.chdir(path)
+
+    @staticmethod
+    def popd():
+        os.chdir(DirUtil.dir_list.pop())
+
+if __name__ == "__main__":
+    DirUtil.pushd('/')
+    print os.getcwd()
+    DirUtil.popd()
+    print os.getcwd()
