7e5ae5b3ac12f69c08b2ecf6be1080a46e357138
iwagaki <iwagaki@users.sourceforge.net>
2012-07-21 21:22:21 +0900

    converted the DiffUtil class as a module

--- diffutil.py
+++ diffutil.py
@@ -6,37 +6,33 @@ import sys
 import tempfile
 import subprocess
 
-class DiffUtil:
-    @staticmethod
-    def write_file(path, src):
-        f = open(path, 'w')
-        try:
-            f.write(src)
-        finally:
-            f.close()
+def write_file(path, src):
+    f = open(path, 'w')
+    try:
+        f.write(src)
+    finally:
+        f.close()
 
-    @staticmethod
-    def get_diff_lines(src, dst):
-        return get_diff(src, dst).splitlines()
+def get_diff_lines(src, dst):
+    return get_diff(src, dst).splitlines()
 
-    @staticmethod
-    def get_diff(src, dst):
-        pid = str(os.getpid())
-        fifo_src = '/tmp/src.' + pid
-        fifo_dst = '/tmp/dst.' + pid
-        os.mkfifo(fifo_src)
-        os.mkfifo(fifo_dst)
+def get_diff(src, dst):
+    pid = str(os.getpid())
+    fifo_src = '/tmp/src.' + pid
+    fifo_dst = '/tmp/dst.' + pid
+    os.mkfifo(fifo_src)
+    os.mkfifo(fifo_dst)
 
-        try:
-            proc = subprocess.Popen('diff ' + fifo_src + ' ' + fifo_dst, shell = True, stdout = subprocess.PIPE)
-            DiffUtil.write_file(fifo_src, src.encode('utf-8'))
-            DiffUtil.write_file(fifo_dst, dst.encode('utf-8'))
-            stdout = proc.communicate()[0].decode('utf-8')
-        finally:
-            os.unlink(fifo_src)
-            os.unlink(fifo_dst)
+    try:
+        proc = subprocess.Popen('diff ' + fifo_src + ' ' + fifo_dst, shell = True, stdout = subprocess.PIPE)
+        write_file(fifo_src, src.encode('utf-8'))
+        write_file(fifo_dst, dst.encode('utf-8'))
+        stdout = proc.communicate()[0].decode('utf-8')
+    finally:
+        os.unlink(fifo_src)
+        os.unlink(fifo_dst)
 
-        return stdout
+    return stdout
 
 if __name__ == '__main__':
-    print DiffUtil.get_diff('abc\n', 'def\n')
+    print get_diff('abc\n', 'def\n')
