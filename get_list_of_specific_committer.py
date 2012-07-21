1b552ae134d2700a6e7d566df088ecc2bb113a12
iwagaki <iwagaki@users.sourceforge.net>
2012-07-21 21:03:53 +0900

    Converted ProcessUtil class as module

--- get_list_of_specific_committer.py
+++ get_list_of_specific_committer.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from processutil import ProcessUtil
+import processutil
 
 def main():
     committer = sys.argv[1]
-    ProcessUtil.run('git log --topo-order --reverse --no-merges --committer=\'%s\' --format=\'\"\%%ci\",%%H,\"%%s\"\'' % committer)
+    processutil.run('git log --topo-order --reverse --no-merges --committer=\'%s\' --format=\'\"\%%ci\",%%H,\"%%s\"\'' % committer)
 
 if __name__ == '__main__':
     main()
--- processutil.py
+++ processutil.py
@@ -6,26 +6,22 @@ import sys
 import subprocess
 import traceback
 
-class ProcessUtil:
-    @staticmethod
-    def get_output(cmd):
-        proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
-        stdout = proc.communicate()[0].decode('utf-8')
-        if not proc.returncode == 0:
-            print 'Waring: return code = ' + str(proc.returncode)
-            traceback.print_stack()
-        return stdout
+def get_output(cmd):
+    proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
+    stdout = proc.communicate()[0].decode('utf-8')
+    if not proc.returncode == 0:
+        print 'Waring: return code = ' + str(proc.returncode)
+        traceback.print_stack()
+    return stdout
 
-    @staticmethod
-    def get_output_lines(cmd):
-        return get_output(cmd).splitlines()
+def get_output_lines(cmd):
+    return get_output(cmd).splitlines()
 
-    @staticmethod
-    def run(cmd, output_file = None):
-        args = { 'shell': True }
-        if output_file:
-            args['stdout'] = output_file
-        return subprocess.call(cmd, **args)
+def run(cmd, output_file = None):
+    args = { 'shell': True }
+    if output_file:
+        args['stdout'] = output_file
+    return subprocess.call(cmd, **args)
 
 if __name__ == '__main__':
-    print ProcessUtil.get_output(' '.join(sys.argv[1:]))
+    print get_output(' '.join(sys.argv[1:]))
