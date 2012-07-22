e4abb2ae7fd9a8a782796c4106ce834ece11aa3f
iwagaki <iwagaki@users.sourceforge.net>
2012-07-22 18:40:43 +0900

    converted the DirUtil class as a module

--- dirutil.py
+++ dirutil.py
@@ -3,20 +3,17 @@
 
 import os
 
-class DirUtil:
-    dir_list = []
+dir_list = []
 
-    @staticmethod
-    def pushd(path):
-        DirUtil.dir_list.append(os.getcwd())
-        os.chdir(path)
+def pushd(path):
+    dir_list.append(os.getcwd())
+    os.chdir(path)
 
-    @staticmethod
-    def popd():
-        os.chdir(DirUtil.dir_list.pop())
+def popd():
+    os.chdir(dir_list.pop())
 
 if __name__ == '__main__':
-    DirUtil.pushd('/')
+    pushd('/')
     print os.getcwd()
-    DirUtil.popd()
+    popd()
     print os.getcwd()
